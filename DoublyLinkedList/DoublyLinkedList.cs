6dc7ca869f5aeb431860b2d375dc93406fc4d093
Naveen Donepudi <ndonepudi@corelogic.com>
2022-12-27 17:47:12 +0530

    27-Dec-22 Intermediate checkin

---
+++ DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,56 @@
+﻿namespace DataStructures_CSharp.DoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        public DoublyLinkedList()
+        {
+            head = null;
+        }
+        private int size = 0;
+        private Node<T>? head, tail = null; 
+
+        public int Count()
+        {
+            return size;
+        }
+
+        public void Clear()
+        {
+            this.head, tail = null;
+            this.size = 0;
+        }
+
+        // Add an element to the tail of the linked list, O(1)
+        public void add(T elem)
+        {
+            addAtLast(elem);
+        }
+
+        private void addAtLast(T? elem)
+        {
+            if(isEmtpy())
+                head = tail = new Node<T>(elem, null, null);
+            else
+            {
+                
+            }
+        }
+
+        public bool isEmtpy()
+        {
+            return size == 0;
+        }
+    }
+
+    public class Node<T>
+    {
+        public T? data;
+        public Node<T>? previous, next;
+        public Node(T? data, Node<T>? previous, Node<T>? next)
+        {
+            this.data = data;
+            this.previous = previous;
+            this.next = next;
+        }
+    }
+}
